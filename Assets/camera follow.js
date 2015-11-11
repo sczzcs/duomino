749c4949e5a1d8744d023088d5949a94a813a36a
sczzcs <jizhiscz@gmail.com>
2015-11-11 18:12:02 +0800

    已完成部分：1.视角旋转。 2.鼠标屏幕边沿摄像机位移。 3.多米诺骨牌创建和清零。

---
+++ Assets/camera follow.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+var cameras : GameObject;
+var speed : float = 0.1;
+
+function Update () {
+
+if(Input.mousePosition.x>=Screen.width){
+cameras.transform.position.x += speed;
+}
+if(Input.mousePosition.x<=0){
+cameras.transform.position.x -= speed;
+}
+if(Input.mousePosition.y>=Screen.height){
+cameras.transform.position.z += speed;
+}
+if(Input.mousePosition.y<=0){ 
+cameras.transform.position.z -= speed;
+}
+
+}
